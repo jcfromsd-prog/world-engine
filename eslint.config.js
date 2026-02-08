d36ff03e2061898428ec00e1a344efe8e69b9794
jcfromsd-prog <jcfromsd@gmail.com>
2026-02-07 19:56:16 -0800

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,7 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true },'no-console': 'off',
       ],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
