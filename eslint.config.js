19b12b289d9aa95f0ce0f38f809dfcb4f34d9fe8
jcfromsd-prog <jcfromsd@gmail.com>
2026-02-07 20:04:17 -0800

    Fix: Define node globals and allow console in ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -24,8 +24,7 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },'no-console': 'off',
-      ],
+        { allowConstantExport: true }],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       'no-console': 'off',
