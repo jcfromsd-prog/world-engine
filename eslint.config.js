6daad1512a7ec63dada4935aea50c71cd8644b29
jcfromsd-prog <jcfromsd@gmail.com>
2026-02-07 17:16:38 -0800

    Fix: Minimal eslint config to disable any-check

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,10 @@
-export default [{ ignores: ["**/*"] }];
+import tseslint from 'typescript-eslint';
+    
+    export default tseslint.config(
+      ...tseslint.configs.recommended,
+      {
+        rules: {
+          '@typescript-eslint/no-explicit-any': 'off',
+        },
+      },
+    );
